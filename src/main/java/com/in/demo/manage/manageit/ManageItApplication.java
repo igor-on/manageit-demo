e6c5e28857c6d8d64b0408549e35b10df5ccf1d1
Igor Nowakowski <igornowakowski24@wp.pl>
2021-07-13 12:37:47 +0200

    Initial commit

---
+++ src/main/java/com/in/demo/manage/manageit/ManageItApplication.java
@@ -0,0 +1,13 @@
+package com.in.demo.manage.manageit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/in/demo/manage/manageit/ManageItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.demo.manage.manageit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
