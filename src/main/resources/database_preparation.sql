ddcbd7f67c2cb984bd979f442a8438080e04dc06
Igor Nowakowski <igornowakowski24@wp.pl>
2021-07-27 18:40:41 +0200

    SQL script changes

--- src/main/resources/database_preparation.sql
+++ src/main/resources/database_preparation.sql
@@ -1,21 +1,17 @@
 USE manage_it_db;
 
-DROP TABLE users;
-DROP TABLE projects;
-DROP TABLE sprints;
-DROP TABLE tasks;
-DROP TABLE sprints_users;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS sprints_users;
+DROP TABLE IF EXISTS sprints;
+DROP TABLE IF EXISTS tasks;
 
-SELECT *
-FROM projects;
-SELECT *
-FROM sprints;
-SELECT *
-FROM tasks;
-SELECT *
-FROM users;
-SELECT *
-FROM sprints_users;
+CREATE TABLE IF NOT EXISTS users
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username   VARCHAR(55) NOT NULL,
+    password   VARCHAR(100) NOT NULL
+    );
 
 CREATE TABLE IF NOT EXISTS sprints
 (
@@ -24,30 +20,22 @@ CREATE TABLE IF NOT EXISTS sprints
     start_date            DATETIME,
     end_date              DATETIME,
     story_points_to_spend INT         NULL,
-    is_active             BOOLEAN,
-    user_id               BIGINT      NOT NULL
-);
+    is_active             BOOLEAN DEFAULT false,
+    user_id               BIGINT      NOT NULL,
+    CONSTRAINT FK_UserSprint1 FOREIGN KEY (user_id) REFERENCES users (id)
+    );
 
 CREATE TABLE IF NOT EXISTS tasks
 (
     id           BIGINT AUTO_INCREMENT PRIMARY KEY,
     name         VARCHAR(55) NOT NULL,
-    description  VARCHAR(255),
+    description  VARCHAR(255) NOT NULL,
     story_points INT,
     progress     ENUM ('TO_DO', 'IN_PROGRESS', 'DONE'),
     priority     ENUM ('1', '2', '3', '4', '5'),
     sprint_id    BIGINT      NOT NULL,
     CONSTRAINT FK_SprintTask FOREIGN KEY (sprint_id) REFERENCES sprints (id)
-);
-
-CREATE TABLE IF NOT EXISTS users
-(
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username   VARCHAR(55) NOT NULL,
-    password   VARCHAR(55),
-    project_id BIGINT      NOT NULL,
-    sprint_id  BIGINT      NOT NULL
-);
+    );
 
 CREATE TABLE IF NOT EXISTS projects
 (
@@ -56,7 +44,7 @@ CREATE TABLE IF NOT EXISTS projects
     description VARCHAR(255),
     owner_id    BIGINT      NOT NULL,
     CONSTRAINT FK_UserProject FOREIGN KEY (owner_id) REFERENCES users (id)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS sprints_users
 (
@@ -64,4 +52,6 @@ CREATE TABLE IF NOT EXISTS sprints_users
     sprints_id BIGINT,
     CONSTRAINT FK_SprintUser FOREIGN KEY (sprints_id) REFERENCES sprints (id),
     CONSTRAINT FK_UserSprint FOREIGN KEY (users_id) REFERENCES users (id)
-);
+    );
+
+
